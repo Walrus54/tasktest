1344bf0e926721f3f3936159533fccea3335d970
Walrus54 <evseevalek5ander@yandex.ru>
2026-08-31 15:45:15 +0700

    Fix ecbinary: vulnerable version now uses ECB + hardcoded key

--- msn-ios-tasks/ecbinary/Cryptor.swift
+++ msn-ios-tasks/ecbinary/Cryptor.swift
@@ -1,57 +1,61 @@
-import CryptoKit
+import CommonCrypto
 import Foundation
-import Security
 
 final class Cryptor {
 
-    private static let keychainAccount = "com.mobsec.ecbinary.aeskey"
-    private static let keyByteCount = 32
-
-    private let key: SymmetricKey
+    private let key: Data
 
     init() {
-        self.key = Cryptor.loadOrCreateKey()
+        self.key = "my-secret-key-123456789012345".data(using: .utf8)!
     }
 
     func encrypt(_ plaintext: Data) -> Data {
-        guard let sealed = try? AES.GCM.seal(plaintext, using: key),
-              let combined = sealed.combined else { return Data() }
-        return combined
+        let keyBytes = [UInt8](key.prefix(kCCKeySizeAES256))
+        let plaintextBytes = [UInt8](plaintext)
+
+        var ciphertextBytes = [UInt8](repeating: 0, count: plaintextBytes.count + kCCBlockSizeAES128)
+        var ciphertextLength: Int = 0
+
+        let status = CCCrypt(
+            CCOperation(kCCEncrypt),
+            CCAlgorithm(kCCAlgorithmAES),
+            CCOptions(kCCOptionECBMode | kCCOptionPKCS7Padding),
+            keyBytes,
+            kCCKeySizeAES256,
+            nil,
+            plaintextBytes,
+            plaintextBytes.count,
+            &ciphertextBytes,
+            ciphertextBytes.count,
+            &ciphertextLength
+        )
+
+        guard status == kCCSuccess else { return Data() }
+        return Data(ciphertextBytes.prefix(ciphertextLength))
     }
 
-    func decrypt(_ combined: Data) -> Data {
-        guard let box = try? AES.GCM.SealedBox(combined: combined),
-              let plaintext = try? AES.GCM.open(box, using: key) else { return Data() }
-        return plaintext
-    }
+    func decrypt(_ ciphertext: Data) -> Data {
+        let keyBytes = [UInt8](key.prefix(kCCKeySizeAES256))
+        let ciphertextBytes = [UInt8](ciphertext)
 
-    private static func loadOrCreateKey() -> SymmetricKey {
-        let query: [String: Any] = [
-            kSecClass as String: kSecClassGenericPassword,
-            kSecAttrAccount as String: keychainAccount,
-            kSecReturnData as String: true,
-            kSecMatchLimit as String: kSecMatchLimitOne
-        ]
-        var item: CFTypeRef?
-        let status = SecItemCopyMatching(query as CFDictionary, &item)
-        if status == errSecSuccess, let data = item as? Data, data.count == keyByteCount {
-            return SymmetricKey(data: data)
-        }
+        var plaintextBytes = [UInt8](repeating: 0, count: ciphertextBytes.count + kCCBlockSizeAES128)
+        var plaintextLength: Int = 0
 
-        var newKey = Data(count: keyByteCount)
-        let genStatus = newKey.withUnsafeMutableBytes {
-            SecRandomCopyBytes(kSecRandomDefault, keyByteCount, $0.baseAddress!)
-        }
-        precondition(genStatus == errSecSuccess, "Unable to generate random AES key")
+        let status = CCCrypt(
+            CCOperation(kCCDecrypt),
+            CCAlgorithm(kCCAlgorithmAES),
+            CCOptions(kCCOptionECBMode | kCCOptionPKCS7Padding),
+            keyBytes,
+            kCCKeySizeAES256,
+            nil,
+            ciphertextBytes,
+            ciphertextBytes.count,
+            &plaintextBytes,
+            plaintextBytes.count,
+            &plaintextLength
+        )
 
-        let addQuery: [String: Any] = [
-            kSecClass as String: kSecClassGenericPassword,
-            kSecAttrAccount as String: keychainAccount,
-            kSecValueData as String: newKey,
-            kSecAttrAccessible as String: kSecAttrAccessibleAfterFirstUnlockThisDeviceOnly
-        ]
-        SecItemDelete(addQuery as CFDictionary)
-        SecItemAdd(addQuery as CFDictionary, nil)
-        return SymmetricKey(data: newKey)
+        guard status == kCCSuccess else { return Data() }
+        return Data(plaintextBytes.prefix(plaintextLength))
     }
 }
